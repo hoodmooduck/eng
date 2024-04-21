c296d7626c3d11a637f72793016fd30566876292
Denis T <goodeyxd@mail.ru>
2024-04-21 14:57:19 +0500

    add translate api

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -7,58 +6,132 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('/')
 })
 
-app.post('/search', (req, res) => {
-  const targetWord = req.body.word.toLowerCase();
+const axios = require('axios');
 
-  fs.readFile('./f.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Ошибка чтения файла: ', err);
-      return;
-    }
-    
-    const lines = data.split('\n');
-    let result = '';
-    let includeTargetWord = false;
-    let search = false;
 
-    
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
-      if(search){
-        break;
-      }
-      if (line.toLowerCase().includes(targetWord)) {
-        includeTargetWord = true;
-        search = true;
-      }
-      if (includeTargetWord) {
-        result += line + `\n`; 
-        const nextLine = lines[i + 1];
-        if (nextLine && nextLine.match(/^[a-z,A-Z]/)) {
-          result += '\n';
-          includeTargetWord = false;
-        }else{
-          break;
-        }
-      }
+app.post('/search', async (req, res) => {
+    const targetWord = req.body.word.toLowerCase();
+    const targetLang = req.body.opt;
+
+    const options = {
+        method: 'POST',
+        url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
+        params: {
+            'to[0]': String(targetLang),
+            'api-version': '3.0',
+            profanityAction: 'NoAction',
+            textType: 'plain'
+        },
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '12c9b46d17mshf348830b4b8224fp16edd4jsnd8562f1e471d',
+            'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+        },
+        data: [
+            {
+                Text: targetWord
+            }
+        ]
+    };
+
+
+
+    try {
+        const response = await axios.request(options);
+        const translatedText = response.data[0].translations[0].text;
+        const result2 = translatedText;
+
+        fs.readFile('./f.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error('Ошибка чтения файла: ', err);
+                return;
+            }
+
+            const lines = data.split('\n');
+            let result = '';
+            let includeTargetWord = false;
+            let search = false;
+
+
+            for (let i = 0; i < lines.length; i++) {
+                const line = lines[i];
+                if (search) {
+                    break;
+                }
+                if (line.toLowerCase().includes(targetWord)) {
+                    includeTargetWord = true;
+                    search = true;
+                }
+                if (includeTargetWord) {
+                    result += line + `\n`;
+                    const nextLine = lines[i + 1];
+                    if (nextLine && nextLine.match(/^[a-z,A-Z]/)) {
+                        result += '\n';
+                        includeTargetWord = false;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            // console.log(result);
+            res.render('result', {result, result2});
+        });
+    } catch (error) {
+        console.error(error);
+        // Если произошла ошибка при запросе к API, сохраняем пустую строку в result2
+        const result2 = '';
+        fs.readFile('./f.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error('Ошибка чтения файла: ', err);
+                return;
+            }
+
+            const lines = data.split('\n');
+            let result = '';
+            let includeTargetWord = false;
+            let search = false;
+
+
+            for (let i = 0; i < lines.length; i++) {
+                const line = lines[i];
+                if (search) {
+                    break;
+                }
+                if (line.toLowerCase().includes(targetWord)) {
+                    includeTargetWord = true;
+                    search = true;
+                }
+                if (includeTargetWord) {
+                    result += line + `\n`;
+                    const nextLine = lines[i + 1];
+                    if (nextLine && nextLine.match(/^[a-z,A-Z]/)) {
+                        result += '\n';
+                        includeTargetWord = false;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            // console.log(result);
+            res.render('result', {result, result2});
+        });
     }
-    // console.log(result);
-    res.render('result', { result });
-  });
 });
 
 app.listen(3000, () => {
-  console.log('Сервер запущен на порту 3000');
+    console.log('Сервер запущен на порту 3000');
 });
